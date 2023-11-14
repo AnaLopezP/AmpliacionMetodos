d6442c8783a9d18523ae7a553bfd0e0c18f118c3
Ana López <anamuyamiga@gmail.com>
2023-11-14 16:59:57 +0100

    fuera de lo común, pero no soy una crack

---
+++ metodoPredictor-Corrector.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#definicion de puntos iniciales y final
+punto_inicial = float(input("Ingrese el punto inicial: "))
+y0 = float(input("Ingrese el valor de y0: "))
+punto_final = float(input("Ingrese el punto final: "))
+n = float(input("Ingrese numero divisiones: ")) #divisiones
+h = (punto_final - punto_inicial) / n #paso
+puntos = []
+
+#funcion 1
+def f1(x, y):
+    return (2-3*x-y)/(x-1)
+def iteracion(x, y, f):
+    while x <= punto_final:
+        x1 = x + h
+        z1 = y + h*f(x, y)
+        y1 = y + (h/2)*(f(x, y) + f(x1, z1))
+        iteracion(x1, y1, f)
+        puntos.append((x1, y1))
+        print(x1, y1)
+        return x1, y1
+
+def grafica(puntos):
+    x = []
+    y = []
+    for i in puntos:
+        x.append(i[0])
+        y.append(i[1])
+    plt.plot(x, y)
+    plt.show()
+    
+iteracion(punto_inicial, y0, f1)
+grafica(puntos)
