19e926f375b0482a5d5fc7d60e2cc4bc472eca19
Ana López <anamuyamiga@gmail.com>
2023-11-30 19:06:23 +0100

    de momento

--- metodoEuler2orden.py
+++ metodoEuler2orden.py
@@ -1,28 +1,31 @@
 #es igual que el método de primer orden, pero se agrega una variable con un CV, formando un vector
 #es el mismo problema pero vectorial
-
 import matplotlib.pyplot as plt
 import numpy as np
+
 #definicion de puntos iniciales y final
 punto_inicial = float(input("Ingrese el punto inicial: "))
-y0 = float(input("Ingrese el valor de y0: "))
+u = float(input("Ingrese el valor inicial de u: "))
+v = float(input("Ingrese el valor inicial de v: "))
 punto_final = float(input("Ingrese el punto final: "))
 n = float(input("Ingrese numero divisiones: ")) #divisiones
 h = (punto_final - punto_inicial) / n #paso
 puntos = []
 puntos_sol = []
-#funcion 1
+
+#funcion
 def f1(x, y):
     return (-2*x*(np.exp(x**2)*y-1)/np.exp(x**2))
 
-def iteracion(x, y, f):
+def iteracion(x, u, v, a, b, f):
     while x <= punto_final:
         x1 = x + h
-        y1 =y + h*f(x, y)
-        iteracion(x1, y1, f)
-        puntos.append((x1, y1))
-        print(x1, y1)
-        return x1, y1
+        u1 = u + h*v
+        v1 = v + h*(-b* u - a*v + f(x))
+        iteracion(x1, u1, v1, a, b, f)
+        puntos.append((x1, u1))
+        print(x1, u1)
+        return x1, u1
 
 def grafica(puntos):
     x = []
@@ -34,14 +37,14 @@ def grafica(puntos):
     plt.show()
     
     
-def iteracion_solucion(x, f):
+'''def iteracion_solucion(x, f):
     while x <= punto_final:
         x1 = x + h
         y1 =h*f(x)
         iteracion_solucion(x1, f)
         puntos_sol.append((x1, y1))
         print(x1, y1)
-        return x1, y1
+        return x1, y1'''
     
 '''def solucion1(x):
     return ((x**2)/np.exp(x**2))
