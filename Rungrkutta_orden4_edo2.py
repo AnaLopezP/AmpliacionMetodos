a42274c7754a268989408a7039f3e94dea432f9d
Ana López <anamuyamiga@gmail.com>
2023-12-05 17:34:28 +0100

    iuoiuoiuioiouoiu

--- Rungrkutta_orden4_edo2.py
+++ Rungrkutta_orden4_edo2.py
@@ -10,16 +10,25 @@ puntos = []
 
 #funcion 1
 def f1(x, u, v):
-    a = None #el coeficiente lineal de y'
-    b = None #el coeficiente de y
+    #a = None #el coeficiente lineal de y'
+    #b = None #el coeficiente de y
     f = None #el termino independiente
-    return a, b, f
+    return f
 
 def iteracion(x, u, v, f, a, b):
-    (k11, k12) = [[0, 1], [-b (x), -a (x)]]*[[u], [v]] + [[0], [f (x)]]
+    '''(k11, k12) = [[0, 1], [-b (x), -a (x)]]*[[u], [v]] + [[0], [f (x)]]
     (k21, k22) = [[0, 1], [-b (x + h/2), -a (x + h/2)]]*[[u + (h/2)*k11], [v + (h/2)*k12]] + [[0], [f (x + h/2)]]
     (k31, k32) = [[0, 1], [-b (x + h/2), -a (x + h/2)]]*[[u + (h/2)*k21], [v + (h/2)*k22]] + [[0], [f (x + h/2)]]
     (k41, k42) = [[0, 1], [-b (x + h), -a (x + h)]]*[[u + h*k31], [v + h*k32]] + [[0], [f (x + h)]]
+    '''
+    k11 = v
+    k12 = f(x, u, v)
+    k21 = v + (h/2)*k11
+    k22 = f(x + h/2, u + (h/2)*k11, v + (h/2)*k12)
+    k31 = v + (h/2)*k21
+    k32 = f(x + h/2, u + (h/2)*k21, v + (h/2)*k22)
+    k41 = v + h*k31
+    k42 = f(x + h, u + h*k31, v + h*k32)
     
     a1 = 1/6
     a2 = 1/3
@@ -28,7 +37,6 @@ def iteracion(x, u, v, f, a, b):
     
     while x <= punto_final:
         x1 = x + h
-        #y1 = y + h*(a1*k1 + a2*k2 + a3*k3 + a4*k4)
         (u1, v1) = u + h*(a1*(k11, k12) + a2*(k21, k22) + a3*(k31, k32) + a4*(k41, k42))
         iteracion(x1, u1, v1, f, a, b)
         puntos.append((x1, u1))
