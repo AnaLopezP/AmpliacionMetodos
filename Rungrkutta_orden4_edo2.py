7eade66738a4a55b08336279268eb3f31608b5e6
Ana López <anamuyamiga@gmail.com>
2023-12-05 16:50:53 +0100

    nuevo archivo

---
+++ Rungrkutta_orden4_edo2.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#definicion de puntos iniciales y final
+punto_inicial = float(input("Ingrese el punto inicial: "))
+y0 = float(input("Ingrese el valor de y0: "))
+punto_final = float(input("Ingrese el punto final: "))
+n = float(input("Ingrese numero divisiones: ")) #divisiones
+h = (punto_final - punto_inicial) / n #paso
+puntos = []
+
+#funcion 1
+def f1(x, y):
+    return (-2*x*(np.exp(x**2)*y-1)/np.exp(x**2))
+
+def iteracion(x, y, f):
+    k1 = f(x, y)
+    k2 = f(x + (h/2), y + (h/2)*k1)
+    k3 = f(x + (h/2), y + (h/2)*k2)
+    k4 = f(x + h, y + h*k3)
+    
+    a1 = 1/6
+    a2 = 1/3
+    a3 = 1/3
+    a4 = 1/6
+    
+    while x <= punto_final:
+        x1 = x + h
+        y1 = y + h*(a1*k1 + a2*k2 + a3*k3 + a4*k4)
+        iteracion(x1, y1, f)
+        puntos.append((x1, y1))
+        print(x1, y1)
+        return x1, y1
+
+def grafica(puntos):
+    x = []
+    y = []
+    for i in puntos:
+        x.append(i[0])
+        y.append(i[1])
+    plt.plot(x, y)
+    plt.show()
+    
+iteracion(punto_inicial, y0, f1)
+grafica(puntos)
