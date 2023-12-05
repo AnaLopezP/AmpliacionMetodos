b330f0376b2ebae141663231070d746de2d8be0b
Ana López <anamuyamiga@gmail.com>
2023-12-05 17:27:07 +0100

    creo que asi pero no se

--- Rungrkutta_orden4_edo2.py
+++ Rungrkutta_orden4_edo2.py
@@ -9,14 +9,17 @@ h = (punto_final - punto_inicial) / n #paso
 puntos = []
 
 #funcion 1
-def f1(x, y):
-    return (-2*x*(np.exp(x**2)*y-1)/np.exp(x**2))
+def f1(x, u, v):
+    a = None #el coeficiente lineal de y'
+    b = None #el coeficiente de y
+    f = None #el termino independiente
+    return a, b, f
 
-def iteracion(x, y, f):
-    k1 = f(x, y)
-    k2 = f(x + (h/2), y + (h/2)*k1)
-    k3 = f(x + (h/2), y + (h/2)*k2)
-    k4 = f(x + h, y + h*k3)
+def iteracion(x, u, v, f, a, b):
+    (k11, k12) = [[0, 1], [-b (x), -a (x)]]*[[u], [v]] + [[0], [f (x)]]
+    (k21, k22) = [[0, 1], [-b (x + h/2), -a (x + h/2)]]*[[u + (h/2)*k11], [v + (h/2)*k12]] + [[0], [f (x + h/2)]]
+    (k31, k32) = [[0, 1], [-b (x + h/2), -a (x + h/2)]]*[[u + (h/2)*k21], [v + (h/2)*k22]] + [[0], [f (x + h/2)]]
+    (k41, k42) = [[0, 1], [-b (x + h), -a (x + h)]]*[[u + h*k31], [v + h*k32]] + [[0], [f (x + h)]]
     
     a1 = 1/6
     a2 = 1/3
@@ -25,11 +28,12 @@ def iteracion(x, y, f):
     
     while x <= punto_final:
         x1 = x + h
-        y1 = y + h*(a1*k1 + a2*k2 + a3*k3 + a4*k4)
-        iteracion(x1, y1, f)
-        puntos.append((x1, y1))
-        print(x1, y1)
-        return x1, y1
+        #y1 = y + h*(a1*k1 + a2*k2 + a3*k3 + a4*k4)
+        (u1, v1) = u + h*(a1*(k11, k12) + a2*(k21, k22) + a3*(k31, k32) + a4*(k41, k42))
+        iteracion(x1, u1, v1, f, a, b)
+        puntos.append((x1, u1))
+        print(x1, u1)
+        return x1, u1
 
 def grafica(puntos):
     x = []
