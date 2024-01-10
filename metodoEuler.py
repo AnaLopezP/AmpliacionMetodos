6c950d92f0a83cf229d606917e8a1c13f998480a
Ana López <anamuyamiga@gmail.com>
2024-01-10 12:14:57 +0100

    no acabo de entender pero bue

--- metodoEuler.py
+++ metodoEuler.py
@@ -12,7 +12,7 @@ puntos_sol = []
 #funcion 1
 #Aqui se pone la funcion F (es decir la y' despejada)
 def f1(x, y):
-    return (x + y)/(x-y)
+    return y - x**2 + 1
 
 def iteracion(x, y, f):
     if x > punto_final:
@@ -35,6 +35,7 @@ def grafica(puntos):
     
 
 #Funcion para iterar con la solucion particular
+#Aquí se itera la solucion con la C calculada. 
 def iteracion_solucion(x, f):
     while x <= punto_final:
         x1 = x + h
@@ -44,13 +45,23 @@ def iteracion_solucion(x, f):
         print(x1, y1)
         return x1, y1
     
-#Funciones particulares con la C calculada. cambio la C según el valor inicial
+#Funciones particulares con la C calculada. cambio la C según el valor inicial. Es decir aquí se pone la y(x). 
 def solucion(x):
-    return ((x**2)/np.exp(x**2))
+    return x**2 + 2*x + 1 - 0.5*np.exp(x)
+def solucion2(x):
+    return x**2 + 2*x + 1 - np.exp(x)
+def solucion3(x):
+    return x**2 + 2*x + 1 - 2*np.exp(x)
 
 
 
 iteracion(punto_inicial, y0, f1)
 grafica(puntos)
-'''iteracion_solucion(punto_inicial, solucion)
-grafica(puntos_sol)'''
+iteracion_solucion(punto_inicial, solucion)
+grafica(puntos_sol)
+puntos_sol = []
+iteracion_solucion(punto_inicial, solucion2)
+grafica(puntos_sol)
+puntos_sol = []
+iteracion_solucion(punto_inicial, solucion3)
+grafica(puntos_sol)
